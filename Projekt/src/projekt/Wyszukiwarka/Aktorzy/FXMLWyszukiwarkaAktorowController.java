98e8ceabf28deec76acd0826a97958e6cac91c9c
BlackHawk <BlackHawk@asus>
2018-01-18 01:10:33 +0100

    Wyszukiwarka aktorów

--- Projekt/src/projekt/Wyszukiwarka/Aktorzy/FXMLWyszukiwarkaAktorowController.java
+++ Projekt/src/projekt/Wyszukiwarka/Aktorzy/FXMLWyszukiwarkaAktorowController.java
@@ -5,15 +5,27 @@
  */
 package projekt.Wyszukiwarka.Aktorzy;
 
+import Algorytmy.Algorytmy;
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import projekt.Model.Aktor;
 import projekt.Model.Film;
+import projekt.Polaczenie.Polaczenie;
 
 /**
  * FXML Controller class
@@ -43,16 +55,99 @@ public class FXMLWyszukiwarkaAktorowController implements Initializable {
     @FXML
     private TableColumn<Film, Integer> columnDlugosc;
     @FXML
-    private TableColumn<?, ?> columnKategoria;
+    private TableColumn<Film, String> columnKategoria;
     @FXML
-    private TableColumn<?, ?> columnOcena;
+    private TableColumn<Film, Double> columnOcena;
+
+    private ObservableList<Film> dataFilm;
+    private ObservableList<Aktor> dataAktor;
+    @FXML
+    private JFXButton bSzukajAktora;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+    }
+
+    @FXML
+    private void wyszukajAktora(ActionEvent event) throws SQLException {
+
+        //dataFilm = FXCollections.observableArrayList();
+        dataAktor = FXCollections.observableArrayList();
+        dataAktor.clear();
+
+        Connection conn = Polaczenie.Connect();
+        Statement ps = conn.createStatement();
+        ResultSet rs = ps.executeQuery("SELECT distinct Aktor.id_aktora,Aktor.imie_aktora,Aktor.nazwisko_aktora,Aktor.nagrody,Aktor.data_urodzenia FROM Aktor,Film_Aktor WHERE Aktor.id_aktora=Film_aktor.id_aktora;");
+
+        while (rs.next()) {
+            if (tfAktor.getText().equals("")) {
+                dataAktor.add(new Aktor(rs.getInt("id_aktora"), rs.getString("imie_aktora"), rs.getString("nazwisko_aktora"), rs.getString("nagrody"), rs.getString("data_urodzenia")));
+
+            } else {
+                String nazwaAktora = rs.getString("imie_aktora") + rs.getString("nazwisko_aktora");
+                if (Algorytmy.porownanieZnakow(nazwaAktora.replace(" ", "").toLowerCase(), tfAktor.getText().replace(" ", "").toLowerCase()) > 83) {
+                    dataAktor.add(new Aktor(rs.getInt("id_aktora"), rs.getString("imie_aktora"), rs.getString("nazwisko_aktora"), rs.getString("nagrody"), rs.getString("data_urodzenia")));
+
+                }
+            }
+
+        }
+
+        ps.close();
+        rs.close();
+        conn.close();
+
+        columnImie.setCellValueFactory(new PropertyValueFactory<>("imie_aktora"));
+        columnNazwisko.setCellValueFactory(new PropertyValueFactory<>("nazwisko_aktora"));
+        columnNagrody.setCellValueFactory(new PropertyValueFactory<>("nagrody"));
+        columnData.setCellValueFactory(new PropertyValueFactory<>("data_urodzenia"));
+
+        tableAktorzy.setItems(null);
+        tableAktorzy.setItems(dataAktor);
+
+    }
+
+    @FXML
+    private void zaznaczAktora(MouseEvent event) throws SQLException {
+        dataFilm = FXCollections.observableArrayList();
+        dataFilm.clear();
+        
+        Connection conn = Polaczenie.Connect();
+        Statement ps = conn.createStatement();
+        ResultSet rs = ps.executeQuery("SELECT distinct film.id_filmu,film.tytuł,film.rezyser,film.dlugosc_filmu,film.gatunek,film.opis, CASE WHEN oceny.ocena is null THEN '0' ELSE AVG(Oceny.ocena) END as ocena  FROM oceny,film,film_aktor,aktor WHERE film_aktor.id_aktora=" + tableAktorzy.getSelectionModel().getSelectedItem().getId_aktora() + " AND film_aktor.id_filmu=film.id_filmu GROUP BY film.id_filmu;");
+
+        while (rs.next()) {
+            
+            dataFilm.add(new Film(rs.getInt("id_filmu"), rs.getString("tytuł"), rs.getString("rezyser"), rs.getInt("dlugosc_filmu"), rs.getString("gatunek"), rs.getDouble("ocena")));
+
+            
+        }
+        
+        
+        ps.close();
+        rs.close();
+        conn.close();
+        
+        columnTytul.setCellValueFactory(new PropertyValueFactory<>("tytuł"));
+        columnRezyser.setCellValueFactory(new PropertyValueFactory<>("rezyser"));
+        columnKategoria.setCellValueFactory(new PropertyValueFactory<>("gatunek"));
+        columnDlugosc.setCellValueFactory(new PropertyValueFactory<>("dlugosc_filmu"));
+        columnOcena.setCellValueFactory(new PropertyValueFactory<>("ocena"));
+
+        tableFilmy.setItems(null);
+        tableFilmy.setItems(dataFilm);
+        
+        
+        
+        
+    }
     
+    
+    
+    
+
 }
